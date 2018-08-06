ce0a26e8aaf989e705478f0070d617affb99411b
mddesignstl <mddesignstl@gmail.com>
2018-08-05 22:03:03 -0500

    add classes for api

--- Album.ts
+++ Album.ts
@@ -1,9 +1,3 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-let Album = /** @class */ (function () {
-    function Album() {
-    }
-    return Album;
-}());
-exports.Album = Album;
-// # sourceMappingURL=Album.js.map
+export class Album {
+    id: number;
+}
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 
 @Injectable()
@@ -17,24 +17,24 @@ export class SpotifyService {
     searchMusic(str: string, type = 'artist') {
         this.searchUrl = 'https://api.spotify.com/v1/search?query=' + str + '&offset=0&limit=20&type=' + type + '&market=US';
         return this._http.get(this.searchUrl)
-            .map(res => res.json());
+            .pipe(map(res => res.json()));
     }
 
     getArtist(id: string) {
         this.artistUrl = 'https://api.spotify.com/v1/artists/' + id;
         return this._http.get(this.artistUrl)
-            .map(res => res.json());
+            .pipe(map(res => res.json()));
     }
 
     getAlbums(artistId: string) {
         this.albumsUrl = 'https://api.spotify.com/v1/artists/' + artistId + '/albums';
         return this._http.get(this.albumsUrl)
-            .map(res => res.json());
+            .pipe(map(res => res.json()));
     }
 
     getAlbum(id: string) {
         this.albumUrl = 'https://api.spotify.com/v1/albums/' + id;
         return this._http.get(this.albumUrl)
-            .map(res => res.json());
+            .pipe(map(res => res.json()));
     }
 }
