f288cf8de19b07a8352bf3f36e440e9da920145f
mddesignstl <mddesignstl@gmail.com>
2018-08-05 21:09:39 -0500

    inject service into appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    providers: [SpotifyService]
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
