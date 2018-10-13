cd2079dce086e494fb05241d09156dacf746bfb7
Keith Spurr <keithspurr@ksqdesign.com>
2018-10-13 20:09:57 +0100

    finalised responsive styling of components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'navbar',
-    templateUrl: 'navbar.component.html'
+    templateUrl: 'navbar.component.html',
+    styleUrls: ['navbar.component.css']
 })
 export class NavbarComponent { }
